9247fc0a0c634a5b5e54d105651125ce210d855a
JonathanLewin <jonathanlewin@hotmail.co.uk>
2023-01-22 18:39:54 -0600

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "freeresourcelocator49625b50": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,11 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'core-js/es/typed-array';
+import 'core-js/es/object';
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
